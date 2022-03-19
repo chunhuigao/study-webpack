a0597654dcdc1045b97cd9844c3cade481ca9bbd
北斗 <1912128546@qq.com>
2022-03-19 09:22:15 +0800

    ✨ feat:支持postcss

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -22,7 +22,18 @@ module.exports = {
       },
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
       },
       {
         test: /\.(less)$/,
