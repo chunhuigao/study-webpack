b4c56a608e915d691cfc63a24ac36062c2e69794
北斗 <1912128546@qq.com>
2022-03-21 20:41:33 +0800

    ✨ feat:修改cnd引入react版本

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react"
 import "./style.scss"
-
+// import { Button } from "antd"
+import { Button } from "antd"
 export default class ComponentName extends Component {
   render() {
-    return <div className="box">我是jddasdssadasdsdssx</div>
+    return (
+      <div className="box">
+        我是jddasdssadasdsdssx
+        <div>
+          <Button>2132</Button>
+        </div>
+      </div>
+    )
   }
 }
